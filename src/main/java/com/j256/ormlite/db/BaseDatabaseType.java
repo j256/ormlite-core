50efa5fd9736b9a761e120625977b3a13936d1b7
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2010-07-07 17:29:12 +0000

    Added comment.

--- src/main/java/com/j256/ormlite/db/BaseDatabaseType.java
+++ src/main/java/com/j256/ormlite/db/BaseDatabaseType.java
@@ -328,6 +328,10 @@ public abstract class BaseDatabaseType implements DatabaseType {
 		return false;
 	}
 
+	/**
+	 * If the field can be nullable, do we need to add some sort of NULL SQL for the create table. By default it is a
+	 * noop. This is necessary because MySQL has a auto default value for the TIMESTAMP type.
+	 */
 	protected void appendCanBeNull(StringBuilder sb, FieldType fieldType) {
 		// default is a noop
 	}
