1df78439b481b86022626c2fc9e3863d313080c9
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2010-08-31 01:57:00 +0000

    Added connection source creation with a helper object instead of the database.

--- src/main/java/com/j256/ormlite/android/AndroidConnectionSource.java
+++ src/main/java/com/j256/ormlite/android/AndroidConnectionSource.java
@@ -3,8 +3,10 @@ package com.j256.ormlite.android;
 import java.sql.SQLException;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.support.DatabaseConnection;
 
 /**
  * Our source for connections to Android databases.
@@ -15,6 +17,11 @@ public class AndroidConnectionSource implements ConnectionSource {
 
 	private AndroidDatabaseConnection readableConnection = null;
 	private AndroidDatabaseConnection readWriteConnection = null;
+	private SQLiteOpenHelper databaseHelper = null;
+
+	public AndroidConnectionSource(SQLiteOpenHelper databaseHelper) {
+		this.databaseHelper = databaseHelper;
+	}
 
 	public AndroidConnectionSource(SQLiteDatabase readWriteDb) {
 		this(readWriteDb, readWriteDb);
@@ -25,12 +32,34 @@ public class AndroidConnectionSource implements ConnectionSource {
 		this.readWriteConnection = new AndroidDatabaseConnection(readWriteDb);
 	}
 
-	public AndroidDatabaseConnection getReadOnlyConnection() throws SQLException {
+	public DatabaseConnection getReadOnlyConnection() throws SQLException {
+		if (readableConnection == null) {
+			synchronized (databaseHelper) {
+				if (readableConnection == null) {
+					readableConnection = new AndroidDatabaseConnection(databaseHelper.getReadableDatabase());
+				}
+			}
+		}
 		return readableConnection;
 	}
 
-	public AndroidDatabaseConnection getReadWriteConnection() throws SQLException {
+	public void releaseReadOnlyConnection(DatabaseConnection connection) throws SQLException {
+		// noop right now
+	}
+
+	public DatabaseConnection getReadWriteConnection() throws SQLException {
+		if (readableConnection == null) {
+			synchronized (databaseHelper) {
+				if (readWriteConnection == null) {
+					readWriteConnection = new AndroidDatabaseConnection(databaseHelper.getWritableDatabase());
+				}
+			}
+		}
 		return readWriteConnection;
+	}
+
+	public void releaseReadWriteConnection(DatabaseConnection connection) throws SQLException {
+		// noop right now
 	}
 
 	/**
