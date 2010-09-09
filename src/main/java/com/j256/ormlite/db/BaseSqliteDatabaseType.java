72edad9ddc33d43cb3b0735267fdf0a09694f59f
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2010-09-09 21:45:15 +0000

    Created the BaseSqliteDatabaseType so android and jdbc can use it.

---
+++ src/main/java/com/j256/ormlite/db/BaseSqliteDatabaseType.java
@@ -0,0 +1,37 @@
+package com.j256.ormlite.db;
+
+import java.util.List;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.FieldType;
+
+/**
+ * Sqlite database type information used to create the tables, etc..
+ * 
+ * <p> NOTE: We need this here because the Android version subclasses it.
+ * 
+ * @author graywatson
+ */
+public abstract class BaseSqliteDatabaseType extends BaseDatabaseType implements DatabaseType {
+
+	@Override
+	protected void configureGeneratedId(StringBuilder sb, FieldType fieldType, List<String> statementsBefore,
+			List<String> additionalArgs, List<String> queriesAfter) {
+		if (fieldType.getDataType() != DataType.INTEGER && fieldType.getDataType() != DataType.INTEGER_OBJ) {
+			throw new IllegalArgumentException("Sqlite requires that auto-increment generated-id be integer types");
+		}
+		sb.append("PRIMARY KEY AUTOINCREMENT ");
+		// no additional call to configureId here
+	}
+
+	@Override
+	public boolean isVarcharFieldWidthSupported() {
+		return false;
+	}
+
+	@Override
+	public boolean isCreateTableReturnsZero() {
+		// 'CREATE TABLE' statements seem to return 1 for some reason
+		return false;
+	}
+}
