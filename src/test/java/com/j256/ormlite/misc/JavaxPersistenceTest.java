6ae4eb27670d5b23559db4a21bd6da08e1dede61
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2010-09-23 15:19:38 +0000

    Added javax test so we can pull it out of the Jdbc tests.

---
+++ src/test/java/com/j256/ormlite/misc/JavaxPersistenceTest.java
@@ -0,0 +1,151 @@
+package com.j256.ormlite.misc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.junit.Test;
+
+import com.j256.ormlite.BaseOrmLiteCoreTest;
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.DatabaseFieldConfig;
+
+public class JavaxPersistenceTest extends BaseOrmLiteCoreTest {
+
+	private static final String STUFF_FIELD_NAME = "notstuff";
+	private static final String JAVAX_ENTITY_NAME = "notjavax";
+
+	@Test
+	public void testConversions() throws Exception {
+		Field[] fields = Javax.class.getDeclaredFields();
+		for (Field field : fields) {
+			DatabaseFieldConfig config = JavaxPersistence.createFieldConfig(databaseType, field);
+			if (field.getName().equals("generatedId")) {
+				assertFalse(config.isId());
+				assertTrue(config.isGeneratedId());
+				assertEquals(field.getName(), config.getFieldName());
+				assertNull(config.getColumnName());
+			} else if (field.getName().equals("id")) {
+				assertTrue(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertEquals(field.getName(), config.getFieldName());
+				assertNull(config.getColumnName());
+			} else if (field.getName().equals("stuff")) {
+				assertFalse(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertEquals(field.getName(), config.getFieldName());
+				assertEquals(STUFF_FIELD_NAME, config.getColumnName());
+			} else if (field.getName().equals("unknown")) {
+				assertFalse(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertEquals(DataType.UNKNOWN, config.getDataType());
+				assertEquals(field.getName(), config.getFieldName());
+				assertNull(config.getColumnName());
+			} else if (field.getName().equals("serializable")) {
+				assertFalse(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertEquals(DataType.SERIALIZABLE, config.getDataType());
+				assertEquals(field.getName(), config.getFieldName());
+				assertNull(config.getColumnName());
+			} else if (field.getName().equals("foreignManyToOne")) {
+				assertFalse(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertTrue(config.isForeign());
+				assertEquals(DataType.UNKNOWN, config.getDataType());
+				assertEquals(field.getName(), config.getFieldName());
+				assertNull(config.getColumnName());
+			} else if (field.getName().equals("foreignOneToOne")) {
+				assertFalse(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertTrue(config.isForeign());
+				assertEquals(DataType.UNKNOWN, config.getDataType());
+				assertEquals(field.getName(), config.getFieldName());
+				assertNull(config.getColumnName());
+			} else {
+				System.err.println("\n\n\nUnknown field: " + field.getName());
+			}
+		}
+	}
+
+	@Test
+	public void testTableName() throws Exception {
+		assertEquals(JAVAX_ENTITY_NAME, JavaxPersistence.getEntityName(Javax.class));
+		assertNull(JavaxPersistence.getEntityName(EntityNoName.class));
+	}
+
+	@Test
+	public void testUpperCaseFieldNames() throws Exception {
+		Field[] fields = Javax.class.getDeclaredFields();
+		UpperCaseFieldDatabaseType ucDatabaseType = new UpperCaseFieldDatabaseType();
+		for (Field field : fields) {
+			DatabaseFieldConfig config = JavaxPersistence.createFieldConfig(ucDatabaseType, field);
+			if (field.getName().equals("id")) {
+				assertTrue(config.isId());
+				assertFalse(config.isGeneratedId());
+				assertEquals("ID", config.getFieldName());
+			}
+		}
+	}
+
+	/* ======================================================================================================= */
+
+	@Entity(name = JAVAX_ENTITY_NAME)
+	protected static class Javax {
+		@Id
+		@GeneratedValue
+		public int generatedId;
+		@Id
+		public int id;
+		@Column(name = STUFF_FIELD_NAME)
+		public String stuff;
+		// this thing is not serializable
+		@Column
+		public Javax unknown;
+		// this is serializable
+		@Column
+		public SerialField serializable;
+		@ManyToOne
+		Foreign foreignManyToOne;
+		@OneToOne
+		Foreign foreignOneToOne;
+		public Javax() {
+		}
+	}
+
+	@Entity
+	protected static class EntityNoName {
+	}
+
+	protected static class SerialField implements Serializable {
+		private static final long serialVersionUID = -3883857119616908868L;
+		String stuff;
+		public SerialField() {
+		}
+	}
+
+	protected static class Foreign {
+		@DatabaseField(generatedId = true)
+		int id;
+		@DatabaseField
+		String stuff;
+	}
+
+	private class UpperCaseFieldDatabaseType extends StubDatabaseType {
+		@Override
+		public boolean isEntityNamesMustBeUpCase() {
+			return true;
+		}
+	}
+}
