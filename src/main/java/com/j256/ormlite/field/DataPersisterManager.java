20bda2b1944a8c9dcf8b94426bea70ceb613d5c4
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-05-19 13:26:28 +0000

    Changed the way the registering and matching works from feedback from Craig.

--- src/main/java/com/j256/ormlite/field/DataPersisterManager.java
+++ src/main/java/com/j256/ormlite/field/DataPersisterManager.java
@@ -1,8 +1,8 @@
 package com.j256.ormlite.field;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.j256.ormlite.field.types.EnumStringType;
@@ -10,32 +10,37 @@ import com.j256.ormlite.field.types.EnumStringType;
 /**
  * Class used to manage the various data types used by the system. The bulk of the data types come from the
  * {@link DataType} enumerated fields although you can also register your own here using the
- * {@link #registerDataTypes(DataPersister...)} method.
+ * {@link #registerDataPersisters(DataPersister...)} method.
  * 
  * @author graywatson
  */
 public class DataPersisterManager {
 
-	private static final DataPersister ENUM_PERSISTER = EnumStringType.getSingleton();
+	private static final DataPersister DEFAULT_ENUM_PERSISTER = EnumStringType.getSingleton();
+
 	private static Map<Class<?>, DataPersister> classMap = null;
+	private static List<DataPersister> registeredPersisters = null;
 
 	/**
 	 * Register a data type with the manager.
 	 */
-	public static void registerDataTypes(DataPersister... dataPersisters) {
-		HashMap<Class<?>, DataPersister> newMap = new HashMap<Class<?>, DataPersister>();
-		if (classMap != null) {
-			newMap.putAll(classMap);
+	public static void registerDataPersisters(DataPersister... dataPersisters) {
+		// we build the map and replace it to lower the chance of concurrency issues
+		List<DataPersister> newList = new ArrayList<DataPersister>();
+		if (registeredPersisters != null) {
+			newList.addAll(registeredPersisters);
 		}
-		for (DataPersister dataPersister : dataPersisters) {
-			if (dataPersister != null) {
-				for (Class<?> clazz : dataPersister.getAssociatedClasses()) {
-					newMap.put(clazz, dataPersister);
-				}
-			}
+		for (DataPersister persister : dataPersisters) {
+			newList.add(persister);
 		}
-		// replace the map to lower the chance of concurrency issues
-		classMap = newMap;
+		registeredPersisters = newList;
+	}
+
+	/**
+	 * Remove any previously persisters that were registered with {@link #registerDataPersisters(DataPersister...)}.
+	 */
+	public static void clear() {
+		registeredPersisters = null;
 	}
 
 	/**
@@ -46,35 +51,49 @@ public class DataPersisterManager {
 	public static DataPersister lookupForClass(Class<?> dataClass) {
 
 		if (classMap == null) {
-			// create an array of persisters for our data-types
-			DataType[] dataTypes = DataType.values();
-			DataPersister[] dataPersisters = new DataPersister[dataTypes.length];
-			for (int i = 0; i < dataPersisters.length; i++) {
-				dataPersisters[i] = dataTypes[i].getDataPersister();
+			// add our built-in persisters the first time around
+			HashMap<Class<?>, DataPersister> newMap = new HashMap<Class<?>, DataPersister>();
+			for (DataType dataType : DataType.values()) {
+				addPersisterToMap(newMap, dataType.getDataPersister());
 			}
-			// this will create a new map
-			registerDataTypes(dataPersisters);
+			classMap = newMap;
 		}
 
 		// first look it up in our map
-		DataPersister dataType = classMap.get(dataClass);
-		if (dataType != null) {
-			return dataType;
+		DataPersister dataPersister = classMap.get(dataClass);
+		if (dataPersister != null) {
+			return dataPersister;
 		}
 
+		// see if the any of the registered persisters are valid
+		if (registeredPersisters != null) {
+			for (DataPersister persister : registeredPersisters) {
+				if (persister.isValidForType(dataClass)) {
+					return persister;
+				}
+			}
+		}
+
+		/*
+		 * Special case for enum types. We can't put this in the registered persisters because we want people to be able
+		 * to override it.
+		 */
 		if (dataClass.isEnum()) {
-			// special handling of the Enum type
-			return ENUM_PERSISTER;
+			return DEFAULT_ENUM_PERSISTER;
 		} else {
+			/*
+			 * Serializable classes return null here because we don't want them to be automatically configured for
+			 * forwards compatibility with future field types that happen to be Serializable.
+			 */
 			return null;
 		}
 	}
 
-	public static Collection<DataPersister> getDataPersisters() {
-		if (classMap == null) {
-			return Collections.emptySet();
-		} else {
-			return classMap.values();
+	private static void addPersisterToMap(HashMap<Class<?>, DataPersister> newMap, DataPersister persister) {
+		if (persister != null) {
+			for (Class<?> clazz : persister.getAssociatedClasses()) {
+				newMap.put(clazz, persister);
+			}
 		}
 	}
 }
