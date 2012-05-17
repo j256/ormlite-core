24b9c991f57d0e7fcde0c1ae5ce942fc621628d9
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2012-05-17 20:45:15 +0000

    Cp.  Some comments, tests.

--- src/test/java/com/j256/ormlite/stmt/mapped/BaseMappedQueryTest.java
+++ src/test/java/com/j256/ormlite/stmt/mapped/BaseMappedQueryTest.java
@@ -8,11 +8,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.field.FieldType;
 import com.j256.ormlite.stmt.BaseCoreStmtTest;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.DatabaseResults;
 
 public class BaseMappedQueryTest extends BaseCoreStmtTest {
@@ -38,5 +42,57 @@ public class BaseMappedQueryTest extends BaseCoreStmtTest {
 		assertNotNull(baseFoo);
 		assertEquals(id, baseFoo.id);
 		verify(results);
+	}
+
+	@Test
+	public void testInnerQueryCacheLookup() throws Exception {
+		Dao<Foo, Object> fooDao = createDao(Foo.class, true);
+		Dao<Outer, Object> outerDao = createDao(Outer.class, true);
+		outerDao.setObjectCache(true);
+		Dao<Inner, Object> innerDao = createDao(Inner.class, true);
+		innerDao.setObjectCache(true);
+
+		Foo foo = new Foo();
+		assertEquals(1, fooDao.create(foo));
+
+		Outer outer1 = new Outer();
+		assertEquals(1, outerDao.create(outer1));
+		Outer outer2 = new Outer();
+		outer2.foreign = foo;
+		assertEquals(1, outerDao.create(outer2));
+
+		Inner inner = new Inner();
+		inner.foreign = foo;
+		assertEquals(1, innerDao.create(inner));
+
+		QueryBuilder<Inner, Object> innerQb = innerDao.queryBuilder();
+		innerQb.selectColumns(Inner.SOME_STRING_FIELD_NAME);
+		QueryBuilder<Outer, Object> qb = outerDao.queryBuilder();
+		List<Outer> results =
+				qb.selectColumns(Outer.SOME_STRING_FIELD_NAME)
+						.where()
+						.in(Outer.SOME_STRING_FIELD_NAME, innerQb)
+						.query();
+		assertEquals(1, results.size());
+	}
+
+	protected static class Outer {
+		public static final String SOME_STRING_FIELD_NAME = "someString";
+		@DatabaseField(generatedId = true)
+		int id;
+		@DatabaseField(columnName = SOME_STRING_FIELD_NAME, foreign = true)
+		Foo foreign;
+		public Outer() {
+		}
+	}
+
+	protected static class Inner {
+		public static final String SOME_STRING_FIELD_NAME = "someString";
+		@DatabaseField(generatedId = true)
+		int id;
+		@DatabaseField(columnName = SOME_STRING_FIELD_NAME, foreign = true)
+		Foo foreign;
+		public Inner() {
+		}
 	}
 }
