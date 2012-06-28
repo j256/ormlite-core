8ceb3563a8d80ad1bf1bd36ecf972474c79b1f83
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2012-06-28 20:25:38 +0000

    Fixed a bug in comparing superclasses of the field with the field.  Thanks to glenviewjeff.  Bug #3538777.

--- src/main/java/com/j256/ormlite/stmt/query/BaseComparison.java
+++ src/main/java/com/j256/ormlite/stmt/query/BaseComparison.java
@@ -15,6 +15,7 @@ import com.j256.ormlite.stmt.SelectArg;
  */
 abstract class BaseComparison implements Comparison {
 
+	private static final String NUMBER_CHARACTERS = "0123456789.-+";
 	protected final String columnName;
 	protected final FieldType fieldType;
 	private final Object value;
@@ -70,10 +71,10 @@ abstract class BaseComparison implements Comparison {
 			// conversion is done when the getValue() is called
 			argHolder.setValue(argOrValue);
 			argList.add(argHolder);
-		} else if (fieldType.isForeign() && fieldType.getType() == argOrValue.getClass()) {
+		} else if (fieldType.isForeign() && fieldType.getType().isAssignableFrom(argOrValue.getClass())) {
 			/*
 			 * If we have a foreign field and our argument is an instance of the foreign object (i.e. not its id), then
-			 * we need to extract the id.
+			 * we need to extract the id. We allow super-classes of the field but not sub-classes.
 			 */
 			FieldType idFieldType = fieldType.getForeignIdField();
 			appendArgOrValue(databaseType, idFieldType, sb, argList, idFieldType.extractJavaFieldValue(argOrValue));
@@ -81,6 +82,21 @@ abstract class BaseComparison implements Comparison {
 			appendSpace = false;
 		} else if (fieldType.isEscapedValue()) {
 			databaseType.appendEscapedWord(sb, fieldType.convertJavaFieldToSqlArgValue(argOrValue).toString());
+		} else if (fieldType.isForeign()) {
+			/*
+			 * I'm not entirely sure this is correct. This is trying to protect against someone trying to pass an object
+			 * into a comparison with a foreign field. Typically if they pass the same field type, then ORMLite will
+			 * extract the ID of the foreign.
+			 */
+			String value = fieldType.convertJavaFieldToSqlArgValue(argOrValue).toString();
+			if (value.length() > 0) {
+				if (NUMBER_CHARACTERS.indexOf(value.charAt(0)) < 0) {
+					throw new SQLException("Foreign field " + fieldType
+							+ " does not seem to be producing a numerical value '" + value
+							+ "'. Maybe you are passing the wrong object to comparison: " + this);
+				}
+			}
+			sb.append(value);
 		} else {
 			// numbers can't have quotes around them in derby
 			sb.append(fieldType.convertJavaFieldToSqlArgValue(argOrValue));
