930b7e2e9a39e6ba5c9fba3f54960498f06c874a
Gray Watson <gray.github@mailnull.com>
2013-03-13 11:41:13 -0400

    Updated release.

--- src/main/java/com/j256/ormlite/misc/VersionUtils.java
+++ src/main/java/com/j256/ormlite/misc/VersionUtils.java
@@ -10,7 +10,7 @@ import com.j256.ormlite.logger.LoggerFactory;
  */
 public class VersionUtils {
 
-	private static final String CORE_VERSION = "VERSION__4.44-SNAPSHOT__";
+	private static final String CORE_VERSION = "VERSION__4.45-SNAPSHOT__";
 
 	private static Logger logger;
 	private static boolean thrownOnErrors = false;
