c5d358ee88b7d2a97205a28ee4ec71c8baf90f50
Gray Watson <gray.github@mailnull.com>
2013-08-05 14:16:33 -0400

    Added additional tests around double removes.

--- src/test/java/com/j256/ormlite/stmt/SelectIteratorTest.java
+++ src/test/java/com/j256/ormlite/stmt/SelectIteratorTest.java
@@ -79,6 +79,23 @@ public class SelectIteratorTest extends BaseCoreStmtTest {
 		}
 	}
 
+	@Test(expected = IllegalStateException.class)
+	public void testIteratorNextRemoveRemoveNoNext() throws Exception {
+		Dao<Foo, Object> dao = createDao(Foo.class, true);
+		Foo foo1 = new Foo();
+		assertEquals(1, dao.create(foo1));
+		Foo foo2 = new Foo();
+		assertEquals(1, dao.create(foo2));
+		CloseableIterator<Foo> iterator = dao.iterator();
+		try {
+			iterator.next();
+			iterator.remove();
+			iterator.remove();
+		} finally {
+			iterator.close();
+		}
+	}
+
 	@Test
 	public void testIteratorRemove() throws Exception {
 		Dao<Foo, Integer> dao = createDao(Foo.class, true);
