59482bab1fd5c63a92b90f4d27a41a375b968b70
André Wachter <andre.w8er@gmail.com>
2014-08-20 12:08:36 +0200

    Allow IS NULL and IS NOT NULL comparisons for all field types.

--- src/main/java/com/j256/ormlite/stmt/query/IsNotNull.java
+++ src/main/java/com/j256/ormlite/stmt/query/IsNotNull.java
@@ -1,31 +1,23 @@
 package com.j256.ormlite.stmt.query;
 
-import java.sql.SQLException;
-import java.util.List;
-
-import com.j256.ormlite.db.DatabaseType;
 import com.j256.ormlite.field.FieldType;
-import com.j256.ormlite.stmt.ArgumentHolder;
 import com.j256.ormlite.stmt.Where;
+
+import java.sql.SQLException;
 
 /**
  * Internal class handling the SQL 'IS NOT NULL' comparison query part. Used by {@link Where#isNull}.
  * 
  * @author graywatson
  */
-public class IsNotNull extends BaseComparison {
+public class IsNotNull extends NullTestBase {
 
 	public IsNotNull(String columnName, FieldType fieldType) throws SQLException {
-		super(columnName, fieldType, null, true);
+		super(columnName, fieldType);
 	}
 
 	@Override
-	public void appendOperation(StringBuilder sb) {
+	protected void appendOperation(StringBuilder sb) {
 		sb.append("IS NOT NULL ");
-	}
-
-	@Override
-	public void appendValue(DatabaseType databaseType, StringBuilder sb, List<ArgumentHolder> argList) {
-		// there is no value
 	}
 }
