b9ae9bcfa4b2b09cc830930d2b19d1179dbc18ee
Gray Watson <gray.github@mailnull.com>
2014-08-26 02:13:43 -0400

    Moved test over from JDBC.

---
+++ src/test/java/com/j256/ormlite/logger/Slf4jLoggingLogTest.java
@@ -0,0 +1,8 @@
+package com.j256.ormlite.logger;
+
+public class Slf4jLoggingLogTest extends BaseLogTest {
+
+	public Slf4jLoggingLogTest() {
+		super(new Slf4jLoggingLog("Slf4jLoggingLogTest"));
+	}
+}
