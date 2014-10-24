23df6710e6d5bacaea03d050606449e29c04ae79
Kirill Maksimov <kirill@jammer.fm>
2014-10-25 00:36:24 +0300

    Pass SQLException#SQLState to next SQLException

--- src/test/java/com/j256/ormlite/misc/SqlExceptionUtilTest.java
+++ src/test/java/com/j256/ormlite/misc/SqlExceptionUtilTest.java
@@ -19,6 +19,18 @@ public class SqlExceptionUtilTest {
 	}
 
 	@Test
+	public void testExceptionWithSQLException() {
+		String sqlReason = "sql exception message";
+		String sqlState = "sql exception state";
+		Throwable cause = new SQLException(sqlReason, sqlState);
+		String msg = "hello";
+		SQLException e = SqlExceptionUtil.create(msg, cause);
+		assertEquals(msg, e.getMessage());
+		assertEquals(sqlState, e.getSQLState());
+		assertEquals(cause, e.getCause());
+	}
+
+	@Test
 	public void testConstructor() throws Exception {
 		@SuppressWarnings({ "rawtypes" })
 		Constructor[] constructors = SqlExceptionUtil.class.getDeclaredConstructors();
