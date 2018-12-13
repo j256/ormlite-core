f80d3e5187124a31e17ab9507f484cbc72a1486d
graynk <gray.nk@gmail.com>
2018-12-13 11:29:28 +0500

    Protected constructors for base class

--- src/main/java/com/j256/ormlite/field/types/BaseLocalDateType.java
+++ src/main/java/com/j256/ormlite/field/types/BaseLocalDateType.java
@@ -3,8 +3,12 @@ package com.j256.ormlite.field.types;
 import com.j256.ormlite.field.SqlType;
 
 public abstract class BaseLocalDateType extends BaseDataType {
-    public BaseLocalDateType(SqlType sqlType, Class<?>[] classes) {
+    protected BaseLocalDateType(SqlType sqlType, Class<?>[] classes) {
         super(sqlType, classes);
+    }
+
+    protected BaseLocalDateType(SqlType sqlType) {
+        super(sqlType);
     }
 
     @Override
