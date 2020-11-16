60049189a6af47729c70c31f67e316a24d24e0ed
Gray Watson <gray.github@mailnull.com>
2020-11-16 13:10:20 -0800

    More TransactionManager fixes to connection and savepoint handling.

--- src/test/java/com/j256/ormlite/misc/TransactionManagerTest.java
+++ src/test/java/com/j256/ormlite/misc/TransactionManagerTest.java
@@ -2,6 +2,7 @@ package com.j256.ormlite.misc;
 
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
@@ -400,6 +401,52 @@ public class TransactionManagerTest extends BaseCoreTest {
 
 		result = dao.queryForId(foo.id);
 		assertNotNull(result);
+	}
+
+	@Test
+	public void testNestedTransactionsReleaseFails() throws Exception {
+		final ConnectionSource connectionSource = createMock(ConnectionSource.class);
+		DatabaseConnection conn = createMock(DatabaseConnection.class);
+		expect(conn.isAutoCommitSupported()).andReturn(true).times(2);
+		expect(conn.isAutoCommit()).andReturn(true).times(2);
+		conn.setAutoCommit(false);
+		expectLastCall().times(2);
+		Savepoint savePoint = createMock(Savepoint.class);
+		expect(savePoint.getSavepointName()).andReturn("name").anyTimes();
+		expect(conn.setSavePoint(isA(String.class))).andReturn(savePoint).times(2);
+		expect(connectionSource.getDatabaseType()).andReturn(databaseType).times(2);
+		expect(connectionSource.getReadWriteConnection(null)).andReturn(conn).times(2);
+		expect(connectionSource.saveSpecialConnection(conn)).andReturn(true);
+		expect(connectionSource.saveSpecialConnection(conn)).andReturn(false);
+		// should only get one of these because we only returned save once
+		connectionSource.clearSpecialConnection(conn);
+		conn.releaseSavePoint(savePoint);
+		expectLastCall().andThrow(new SQLException("testing the release to fail"));
+		conn.rollback(savePoint);
+		expectLastCall().times(2);
+		connectionSource.releaseConnection(conn);
+		expectLastCall().times(2);
+		conn.setAutoCommit(true);
+		expectLastCall().times(2);
+
+		replay(connectionSource, conn, savePoint);
+		try {
+			TransactionManager.callInTransaction(connectionSource, new Callable<Void>() {
+				@Override
+				public Void call() throws SQLException {
+					TransactionManager.callInTransaction(connectionSource, new Callable<Void>() {
+						@Override
+						public Void call() {
+							return null;
+						}
+					});
+					return null;
+				}
+			});
+		} catch (SQLException se) {
+			// expected
+		}
+		verify(connectionSource, conn, savePoint);
 	}
 
 	private void testTransactionManager(TransactionManager mgr, final Exception exception,
