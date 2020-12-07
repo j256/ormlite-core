fff5f1cbd5897f23eaae9b89d1b5150191c810a1
Gray Watson <gray.github@mailnull.com>
2020-12-07 09:47:17 -0500

    Fixed some race conditions.

--- src/test/java/com/j256/ormlite/table/SchemaUtilsTest.java
+++ src/test/java/com/j256/ormlite/table/SchemaUtilsTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.j256.ormlite.BaseCoreTest;
@@ -13,48 +14,66 @@ import com.j256.ormlite.h2.H2DatabaseType;
 
 public class SchemaUtilsTest extends BaseCoreTest {
 
+	private final static String SCHEMA_NAME = "schema";
+
+	@Override
+	@Before
+	public void before() throws Exception {
+		super.before();
+	}
+
 	@Test
 	public void testCreateSchema() throws SQLException {
+		SchemaUtils.dropSchema(connectionSource, Schema.class, true);
 		assertTrue(SchemaUtils.createSchema(connectionSource, Schema.class) > 0);
 	}
 
 	@Test
 	public void testCreateSchemaName() throws SQLException {
-		assertTrue(SchemaUtils.createSchema(connectionSource, "schema") > 0);
+		SchemaUtils.dropSchema(connectionSource, SCHEMA_NAME, true);
+		assertTrue(SchemaUtils.createSchema(connectionSource, SCHEMA_NAME) > 0);
 	}
 
 	@Test
 	public void testCreateSchemaDao() throws SQLException {
 		Dao<Schema, Integer> dao = createDao(Schema.class, false);
+		SchemaUtils.dropSchema(connectionSource, Schema.class, true);
 		assertTrue(SchemaUtils.createSchema(dao) > 0);
 	}
 
 	@Test
 	public void testCreateIfNotExists() throws SQLException {
+		SchemaUtils.dropSchema(connectionSource, Schema.class, true);
+		SchemaUtils.dropSchema(connectionSource, SCHEMA_NAME, true);
 		assertTrue(SchemaUtils.createSchemaIfNotExists(connectionSource, Schema.class) > 0);
-		assertTrue(SchemaUtils.createSchemaIfNotExists(connectionSource, "schema") > 0);
+		assertTrue(SchemaUtils.createSchemaIfNotExists(connectionSource, SCHEMA_NAME) > 0);
 	}
 
 	@Test
 	public void testCreateSchemaStatements() throws SQLException {
-		assertTrue(SchemaUtils.getCreateSchemaStatements(new H2DatabaseType(), "schema").size() > 0);
+		assertTrue(SchemaUtils.getCreateSchemaStatements(new H2DatabaseType(), SCHEMA_NAME).size() > 0);
 	}
 
 	@Test
 	public void testDropSchema() throws SQLException {
+		SchemaUtils.dropSchema(connectionSource, Schema.class, true);
 		assertTrue(SchemaUtils.createSchema(connectionSource, Schema.class) > 0);
 		assertTrue(SchemaUtils.dropSchema(connectionSource, Schema.class, true) > 0);
 		assertTrue(SchemaUtils.createSchema(connectionSource, Schema.class) > 0);
 		assertTrue(SchemaUtils.dropSchema(connectionSource, Schema.class, true) > 0);
+		// this will generate errors but we ignore them
 		assertTrue(SchemaUtils.dropSchema(connectionSource, Schema.class, true) > 0);
 	}
 
 	@Test(expected = SQLException.class)
 	public void testDropSchemaThrow() throws SQLException {
+		// make sure it is gone
+		SchemaUtils.dropSchema(connectionSource, Schema.class, true);
+		// now drop it while _not_ ignoring errors
 		SchemaUtils.dropSchema(connectionSource, Schema.class, false);
 	}
 
-	@DatabaseTable(tableName = "table", schemaName = "schema")
+	@DatabaseTable(tableName = "table", schemaName = SCHEMA_NAME)
 	protected static class Schema {
 		@DatabaseField(generatedId = true)
 		public int id;
@@ -62,6 +81,7 @@ public class SchemaUtilsTest extends BaseCoreTest {
 		public int val;
 
 		public Schema() {
+			// needed for ormlite
 		}
 	}
 }
