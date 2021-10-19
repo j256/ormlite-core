4d6a911b31d022bc59e60931fde453f6c3cd5d26
Mingqing Sun <44515552+NanaOkada@users.noreply.github.com>
2021-10-18 22:53:02 -0500

    Fix flaky testCanBeNull in FieldTypeTest.java (#228)
    * fix flaky test testAssignForeign
    
    * fix flaky testNotNull
    
    * Update VersionUtils.java
    
    Co-authored-by: Sun <ms19@fa21-cs527-067.cs.illinois.edu>

--- src/test/java/com/j256/ormlite/field/FieldTypeTest.java
+++ src/test/java/com/j256/ormlite/field/FieldTypeTest.java
@@ -18,6 +18,8 @@ import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -426,12 +428,17 @@ public class FieldTypeTest extends BaseCoreTest {
 	@Test
 	public void testCanBeNull() throws Exception {
 		Field[] fields = CanBeNull.class.getDeclaredFields();
+		Arrays.sort(fields, new Comparator<Field>() {
+			public int compare(Field a, Field b) {
+				return b.getName().compareTo(a.getName());
+			}
+		});
 		assertTrue(fields.length >= 2);
-		Field field = fields[0];
+		Field field = fields[1];
 		FieldType fieldType =
 				FieldType.createFieldType(databaseType, CanBeNull.class.getSimpleName(), field, CanBeNull.class);
 		assertTrue(fieldType.isCanBeNull());
-		field = fields[1];
+		field = fields[0];
 		fieldType = FieldType.createFieldType(databaseType, CanBeNull.class.getSimpleName(), field, CanBeNull.class);
 		assertFalse(fieldType.isCanBeNull());
 	}
