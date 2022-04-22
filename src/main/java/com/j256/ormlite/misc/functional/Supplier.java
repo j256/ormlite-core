394caefb33baa704be154380edfce45db1dce019
BlackBaroness <shoyurash2@gmail.com>
2022-04-22 22:16:21 +0300

    Supplier docs improvements

--- src/main/java/com/j256/ormlite/misc/functional/Supplier.java
+++ src/main/java/com/j256/ormlite/misc/functional/Supplier.java
@@ -1,14 +1,18 @@
 package com.j256.ormlite.misc.functional;
 
 /**
- * This is a functional interface intended to support lambda expressions in ormlite.
+ * Represents a supplier of results.
+ *
+ * There is no requirement that a new or distinct result be returned each time the supplier is invoked.
+ *
+ * This is a functional interface intended to support lambda expressions in ormlite whose functional method is get().
  */
 public interface Supplier<T> {
 
     /**
-     * Gets a value.
+     * Gets a result.
      *
-     * @return a value
+     * @return a result
      */
     T get();
 }
