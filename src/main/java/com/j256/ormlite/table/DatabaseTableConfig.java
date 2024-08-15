78d01609c0d042925f0670590c48c3cf97ee2024
Gray Watson <gray.github@mailgw.com>
2024-08-14 22:57:33 -0400

    cp

--- src/main/java/com/j256/ormlite/table/DatabaseTableConfig.java
+++ src/main/java/com/j256/ormlite/table/DatabaseTableConfig.java
@@ -3,10 +3,8 @@ package com.j256.ormlite.table;
 import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import com.j256.ormlite.db.DatabaseType;
 import com.j256.ormlite.field.DatabaseField;
